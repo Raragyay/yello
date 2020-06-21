4f6584e2812f5bdfcfc7e640ed5979710f2c3c13
Ozan Ürkmez <arkantos250@gmail.com>
2020-06-21 19:47:46 +0300

    IMPLEMENT ARGUMENT PASSING MWHAHAH

--- server/main.go
+++ server/main.go
@@ -183,7 +183,7 @@ func reader(conn *websocket.Conn) {
 
 		if flag == ok {
 			//2 fields message!
-			dataString := string(data)
+			dataString := fields[0] + " " + fields[1]
 			specification := clientCallSpecification{
 				isDirectMessage: true,
 				msgBase:         dataString,
@@ -203,7 +203,19 @@ func reader(conn *websocket.Conn) {
 			handleDisconnectPlayer(p)
 			panic("PONG INVALID")
 		} else if len(fields) == 3 { //this is for directive and argument
-
+			dataString := fields[0] + " " + fields[1]
+			specification := clientCallSpecification{
+				isDirectMessage: false,
+				msgBase:         dataString,
+			}
+			if val, ok := handledClientCalls[specification]; ok {
+				val(&playerRequest{message: dataString, p: p}, fields[2])
+			} else {
+				p.m.RUnlock()
+				p.writeChanneledMessage("PONG INVALID")
+				handleDisconnectPlayer(p)
+				panic("PONG INVALID DIRECT MESSAGE: " + string(data))
+			}
 		} else {
 			//invalid number of fields boi! GET OUTTA MY SERVER YE DEGENERATE
 			p.m.RUnlock()
