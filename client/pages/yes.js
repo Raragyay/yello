ccf5fa60b2ed7ce4c1b405f32875292007fb5801
Ozan Ürkmez <arkantos250@gmail.com>
2020-06-21 11:13:58 +0300

    fix indent

--- client/pages/yes.js
+++ client/pages/yes.js
@@ -2,19 +2,19 @@ let socket = new WebSocket("ws:http://185.163.47.170:5000/ws");
 console.log("Attempting Connection...");
 
 socket.onopen = () => {
-        console.log("Successfully Connected");
-        socket.send("Hi From the Client!")
-    };
+    console.log("Successfully Connected");
+    socket.send("Hi From the Client!")
+};
         
-    socket.onclose = event => {
+socket.onclose = event => {
     console.log("Socket Closed Connection: ", event);
-    socket.send("Client Closed!")
-    };
+ 	socket.send("Client Closed!")
+};
 
-    socket.onmessage = (msg) =>{
-    	console.log(msg);
-    }
+socket.onmessage = (msg) =>{
+    console.log(msg);
+}
 
-    socket.onerror = error => {
-        console.log("Socket Error: ", error);
-    };
+socket.onerror = error => {
+    console.log("Socket Error: ", error);
+};
