df5f543902c244ed0ad6c26d1e9c918b68d7bdc8
Ozan Ürkmez <arkantos250@gmail.com>
2020-06-21 04:10:31 +0300

    log

--- server/sockets.go
+++ server/sockets.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"log"
+
 	socketio "github.com/googollee/go-socket.io"
 )
 
 func handleQueueSockets(s socketio.Conn) error {
-
+	log.Println(s.RemoteAddr().String() + ": socket connection established.")
 	return nil
 }
