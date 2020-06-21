4c588efe9b4681441cd39ef665f80cc16434ec02
Raragyay <let987let987@gmail.com>
2020-06-20 23:52:58 -0400

    Refactored to allow more modularbility with json specifications.

--- client/render/p5canvas.js
+++ client/render/p5canvas.js
@@ -1,11 +1,10 @@
 //import {background, createCanvas, loadImage, windowHeight, windowWidth} from "p5/global";
 
-let levelImg;
 let pellets = [];
 let entities = [];
 let level, isWall;
 loadJSON('./levels/level1.json', x => {
-    level = x
+    level = x.levelData
     isWall = level.map(x => x.map(tile => tile === '100'))
 })
 let levelHeight, levelWidth;
@@ -29,7 +28,7 @@ function loadJSON(filePath, success, error) {
 }
 
 function preload() {
-    levelImg = loadImage('images/level.png')
+    // levelImg = loadImage('images/level.png')
     // level = levelStr.split('\n').map(str => str.split(' '))
     // print(data)
     // print(isWall)
@@ -37,13 +36,14 @@ function preload() {
 
 function calc_block_size() {
     levelHeight = windowHeight / 2
-    levelWidth = windowHeight / 2 / levelImg.height * levelImg.width
-    block_size = levelHeight / 25
+    levelWidth = windowHeight / 2 / level.length * level[0].length
+    block_size = levelHeight / level.length
 }
 
 function setup() {
     calc_block_size();
-    createCanvas(levelWidth, levelHeight);
+    var canvas=createCanvas(levelWidth, levelHeight);
+    canvas.parent('sketch-div')
 }
 
 function windowResized() {
@@ -52,18 +52,17 @@ function windowResized() {
 }
 
 
-function draw() {
-    background(255);
+function drawLevel() {
     fill(255, 204, 0)
     noStroke()
-    for (let i = 0; i < 25; i++) {
-        for (let j = 0; j < 21; j++) {
+    for (let i = 0; i < isWall.length; i++) {
+        for (let j = 0; j < isWall[0].length; j++) {
             if (isWall[i][j]) {
                 let border_checks = []
                 border_checks[0] = (j > 0 && isWall[i][j - 1])
                 border_checks[1] = (i > 0 && isWall[i - 1][j])
-                border_checks[2] = (j < 21 - 1 && isWall[i][j + 1])
-                border_checks[3] = (i < 25 - 1 && isWall[i + 1][j])
+                border_checks[2] = (j < isWall[0].length - 1 && isWall[i][j + 1])
+                border_checks[3] = (i < isWall.length - 1 && isWall[i + 1][j])
                 let corner_checks = []
                 for (let k = 0; k < 4; k++) {
                     corner_checks[k] = !((border_checks[k] || border_checks[(k + 1) % 4])) * 10
@@ -72,7 +71,9 @@ function draw() {
             }
         }
     }
+}
 
-    // background(levelImg);
-    // x++
+function draw() {
+    background(255);
+    drawLevel();
 }
