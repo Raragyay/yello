9641220488590ed62f1e83b3890703dd950bef7f
Ozan Ürkmez <arkantos250@gmail.com>
2020-06-21 11:11:43 +0300

    big yes

--- client/pages/yes.js
+++ client/pages/yes.js
@@ -1,1 +1,20 @@
-const socket = io('http://185.163.47.170:5000/');
+let socket = new WebSocket("ws:http://185.163.47.170:5000/ws");
+console.log("Attempting Connection...");
+
+socket.onopen = () => {
+        console.log("Successfully Connected");
+        socket.send("Hi From the Client!")
+    };
+        
+    socket.onclose = event => {
+    console.log("Socket Closed Connection: ", event);
+    socket.send("Client Closed!")
+    };
+
+    socket.onmessage = (msg) =>{
+    	console.log(msg);
+    }
+
+    socket.onerror = error => {
+        console.log("Socket Error: ", error);
+    };
